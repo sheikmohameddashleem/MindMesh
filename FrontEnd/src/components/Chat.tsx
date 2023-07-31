bf2b78d6d5b469e2356f032cab7846249dfc6dd6
sheik kalibullah <121329816+sheikmohameddashleem@users.noreply.github.com>
2023-07-31 12:38:55 +0530

    done

--- FrontEnd/src/components/Chat.tsx
+++ FrontEnd/src/components/Chat.tsx
@@ -1,7 +1,6 @@
-import { useState ,useRef, useEffect} from "react";
-import {startInterview, submitAnswer} from "../redux/action"
-import { useNavigate } from 'react-router-dom';
-
+import { useState, useRef, useEffect } from "react";
+import { startInterview, submitAnswer } from "../redux/action";
+import { useNavigate } from "react-router-dom";
 
 declare global {
   interface Window {
@@ -9,46 +8,57 @@ declare global {
   }
 }
 
-const Chat = (props : any) => {
+const Chat = (props: any) => {
   const navigate = useNavigate();
-  const [interviewDone , setInterviewDone] = useState(false)
-const [submitCalls , setSubmitCall] = useState(0)
-  const { courseName } = props
-  const [messages, setMessages] = useState<any[]>([{
-      "time" : new Date(),
-      "creator": "ai",
-      "message" : "Welcome!!"
-  }]);
+  const [interviewDone, setInterviewDone] = useState(false);
+  const [submitCalls, setSubmitCall] = useState(0);
+  const { courseName } = props;
+  const [messages, setMessages] = useState<any[]>([
+    {
+      time: new Date(),
+      creator: "ai",
+      message: "Welcome!!",
+    },
+  ]);
 
   useEffect(() => {
     startInterview(courseName)
-  .then((response :any) => {
-    console.log('Response:', response.data.message);
-    setMessages((prev)=>[...prev, {"time" : new Date().getTime(),
-      "creator": "ai",
-      "message" :response.data.message + "?"}])
-  })
-  .catch((error :any) => {
-    console.error('Error:', error);
-  });
-  },[])
-  
-    const el = useRef(null);
+      .then((response: any) => {
+        console.log("Response:", response.data.message);
+        setMessages((prev) => [
+          ...prev,
+          {
+            time: new Date().getTime(),
+            creator: "ai",
+            message: response.data.message + "?",
+          },
+        ]);
+      })
+      .catch((error: any) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  const el = useRef(null);
 
   useEffect(() => {
     if (el.current) {
-    (el.current as HTMLElement).scrollIntoView({ block: "start", behavior: "smooth" });
-  }
-  }, [messages])
+      (el.current as HTMLElement).scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
 
   const [isListening, setIsListening] = useState(false);
-  const [recognizedText, setRecognizedText] = useState('');
-  const [conversation, setConversation] = useState('');
+  const [recognizedText, setRecognizedText] = useState("");
+  const [conversation, setConversation] = useState("");
 
   const recognitionRef = useRef(null);
 
   const handleStartListening = () => {
-    const recognition : any = recognitionRef.current || new window.webkitSpeechRecognition(); // For Chrome
+    const recognition: any =
+      recognitionRef.current || new window.webkitSpeechRecognition(); // For Chrome
     // const recognition = recognitionRef.current || new window.SpeechRecognition(); // For other browsers
 
     recognition.continuous = true;
@@ -56,22 +66,22 @@ const [submitCalls , setSubmitCall] = useState(0)
 
     recognition.onstart = () => {
       setIsListening(true);
-      setRecognizedText('');
+      setRecognizedText("");
     };
 
-    recognition.onresult = (event:any) => {
-      let transcript = '';
+    recognition.onresult = (event: any) => {
+      let transcript = "";
       for (let i = event.resultIndex; i < event.results.length; i++) {
         const { transcript: result } = event.results[i][0];
-        transcript += result + ' ';
+        transcript += result + " ";
       }
 
       setRecognizedText(transcript);
-      setConversation(prevConversation => prevConversation + transcript);
+      setConversation((prevConversation) => prevConversation + transcript);
     };
 
-    recognition.onerror = (event :any) => {
-      console.error('Error occurred in recognition: ', event.error);
+    recognition.onerror = (event: any) => {
+      console.error("Error occurred in recognition: ", event.error);
       setIsListening(false);
     };
 
@@ -86,54 +96,62 @@ const [submitCalls , setSubmitCall] = useState(0)
     recognitionRef.current = recognition;
   };
 
-  const handleStopListening = (text : string) => {
+  const handleStopListening = (text: string) => {
     if (isListening) {
       const recognition = recognitionRef.current;
       if (recognition) {
         //@ts-ignore
-          recognition.stop();
+        recognition.stop();
       }
-      }
+    }
     let conversationObj = {
-      "time" : Date.now(),
-      "creator": "user",
-      "message" : text
-    }
-   
-    setMessages((prev) => [...prev, conversationObj])
+      time: Date.now(),
+      creator: "user",
+      message: text,
+    };
+
+    setMessages((prev) => [...prev, conversationObj]);
     const answer = {
-       "answer": conversation
-    }
+      answer: conversation,
+    };
 
-    setSubmitCall((prev)=> prev+1)
+    setSubmitCall((prev) => prev + 1);
 
-    console.log("submitCalls count" , submitCalls)
-  submitAnswer(answer ,submitCalls)
-  .then((response : any) => {
-    console.log('Response after submit:', response.data);
-   
-      setMessages((prev)=>[...prev, {"time" : new Date().getTime(),
-      "creator": "ai",
-        "message": response.data.msg 
-      }])
-    setRecognizedText('');
+    console.log("submitCalls count", submitCalls);
+    submitAnswer(answer, submitCalls)
+      .then((response: any) => {
+        console.log("Response after submit:", response.data);
 
-     /// Checking if there is score available in the response.data
+        setMessages((prev) => [
+          ...prev,
+          {
+            time: new Date().getTime(),
+            creator: "ai",
+            message: response.data.message,
+          },
+        ]);
+        setRecognizedText("");
 
-    if (response.data.hasOwnProperty('score')) {
-      console.log("yes ended", "response.data.hasOwnProperty('score')", response.data.hasOwnProperty('score'))
-      console.log("score is", response.data.score)
-      localStorage.setItem("score", JSON.stringify(response.data.score))
-       setInterviewDone(true)
-    }
-  })
-  .catch((error:any) => {
-    console.error('Error:', error);
-  });
-      console.log("conversation", messages)
+        /// Checking if there is score available in the response.data
+
+        if (response.data.hasOwnProperty("score")) {
+          console.log(
+            "yes ended",
+            "response.data.hasOwnProperty('score')",
+            response.data.hasOwnProperty("score")
+          );
+          console.log("score is", response.data.score);
+          localStorage.setItem("score", JSON.stringify(response.data.score));
+          setInterviewDone(true);
+        }
+      })
+      .catch((error: any) => {
+        console.error("Error:", error);
+      });
+    console.log("conversation", messages);
   };
- 
-      // Calculate the number of newlines in the conversation to increment the rows
+
+  // Calculate the number of newlines in the conversation to increment the rows
   useEffect(() => {
     const numNewLines = (conversation.match(/\n/g) || []).length;
     const conversationRows = Math.min(2 + numNewLines, 10); // Limit to a maximum of 10 rows
@@ -141,62 +159,117 @@ const [submitCalls , setSubmitCall] = useState(0)
   }, [conversation]);
 
   const [rows, setRows] = useState(2);
- 
-   
 
   return (
-    <div style={{maxWidth:"100%"}}>
-      <div style={{ overscrollBehavior: 'auto', height: '100vh' }}>
+    <div style={{ maxWidth: "100%" }}>
+      <div style={{ overscrollBehavior: "auto", height: "100vh" }}>
         <div className="fixed w-full bg-slate-600	text-center h-16 pt-2 text-white flex justify-between shadow-md top-0">
-          <div className="my-3 text-green-100 font-bold text-lg tracking-wide text-center">@MindMesh</div>
+          <div className="my-3 text-green-100 font-bold text-lg tracking-wide text-center">
+            @MindMesh
+          </div>
         </div>
 
-        <div className="mt-20 mb-16" style={{ maxHeight: 'calc(100vh - 6rem)', overflowY: 'auto' ,  width:"100%" , marginRight:"auto", marginLeft:"auto"}}>
+        <div
+          className="mt-20 mb-16"
+          style={{
+            maxHeight: "calc(100vh - 6rem)",
+            overflowY: "auto",
+            width: "100%",
+            marginRight: "auto",
+            marginLeft: "auto",
+          }}
+        >
           {messages?.map((message) => (
-            <div ref={el} className="clearfix" style={{ display:"flex", justifyContent: message.creator === "user" ? "flex-end" : "flex-start", width:"75%", margin:"auto"}} key={message.time}>
-                      
-                <img
-                src={message.creator === "user"
-              ? "https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector.png"// Replace with the URL of the current user's avatar image
-              : 
-                "https://upload.wikimedia.org/wikipedia/en/b/bd/Reddit_Logo_Icon.svg"}
-              alt="Avatar"
-              className="w-12 h-11 rounded-full float-left mx-4 my-2"
-            /> 
-                      
-              
-            <div className="w-2.5/4 mx-4 my-2 p-2 rounded-lg text-left"  style={{backgroundColor: message.creator === "user" ? "#C0BEFE" :"#E6E4E3"}} >
-              {message.message}
-                    </div>
-                      
-          </div>
-        ))}
+            <div
+              ref={el}
+              className="clearfix"
+              style={{
+                display: "flex",
+                justifyContent:
+                  message.creator === "user" ? "flex-end" : "flex-start",
+                width: "75%",
+                margin: "auto",
+              }}
+              key={message.time}
+            >
+              <img
+                src={
+                  message.creator === "user"
+                    ? "https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector.png" // Replace with the URL of the current user's avatar image
+                    : "https://upload.wikimedia.org/wikipedia/en/b/bd/Reddit_Logo_Icon.svg"
+                }
+                alt="Avatar"
+                className="w-12 h-11 rounded-full float-left mx-4 my-2"
+              />
+
+              <div
+                className="w-2.5/4 mx-4 my-2 p-2 rounded-lg text-left"
+                style={{
+                  backgroundColor:
+                    message.creator === "user" ? "#C0BEFE" : "#E6E4E3",
+                }}
+              >
+                {message.message}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      {interviewDone ? <div style={{ display:"flex" ,justifyContent:"center", alignItems:"center"}} onClick={() => {
-        navigate("/score")
-      }}>
-        <div id="toast-success" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-        </svg>
-        <span className="sr-only">Check icon</span>
-    </div>
-    <div className="ml-3 text-sm font-normal">Interview completed successfully. Click to view score.</div>
-</div>
-      </div> :
-        <div className="fixed w-full flex justify-between bg-slate-600" style={{ bottom: '0px' }}>
-          <textarea id="convert_text"
+      {interviewDone ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onClick={() => {
+            navigate("/score");
+          }}
+        >
+          <div
+            id="toast-success"
+            className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+            role="alert"
+          >
+            <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+              </svg>
+              <span className="sr-only">Check icon</span>
+            </div>
+            <div className="ml-3 text-sm font-normal">
+              Interview completed successfully. Click to view score.
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div
+          className="fixed w-full flex justify-between bg-slate-600"
+          style={{ bottom: "0px" }}
+        >
+          <textarea
+            id="convert_text"
             className="flex-grow m-2 mr-1 rounded-full border border-zinc-700 text-center resize-none"
             placeholder="Your answer.."
             rows={rows}
             cols={50}
             readOnly
             value={recognizedText}
-            style={{ outline: 'none', minHeight: '2rem' }}
+            style={{ outline: "none", minHeight: "2rem" }}
           />
-          <button className="m-2 p-2 rounded-full bg-slate-600 focus:outline-none" id="click_to_convert" onClick={handleStartListening} disabled={isListening} style={{ backgroundColor: isListening ? "red" : "grey" }}>
+          <button
+            className="m-2 p-2 rounded-full bg-slate-600 focus:outline-none"
+            id="click_to_convert"
+            onClick={handleStartListening}
+            disabled={isListening}
+            style={{ backgroundColor: isListening ? "red" : "grey" }}
+          >
             <svg
               className="text-white w-8 h-8"
               fill="none"
@@ -219,7 +292,12 @@ const [submitCalls , setSubmitCall] = useState(0)
             </svg>
           </button>
 
-          <button className="m-2" style={{ outline: 'none' }} onClick={() => handleStopListening(recognizedText)} disabled={!isListening} >
+          <button
+            className="m-2"
+            style={{ outline: "none" }}
+            onClick={() => handleStopListening(recognizedText)}
+            disabled={!isListening}
+          >
             <svg
               className="svg-inline--fa bg-slate-600 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2"
               aria-hidden="true"
@@ -236,8 +314,8 @@ const [submitCalls , setSubmitCall] = useState(0)
               />
             </svg>
           </button>
-              
-        </div>}
+        </div>
+      )}
     </div>
   );
 };
